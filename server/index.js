3dbd49d855312a42168f0599f8602fa7d234c283
Sudhanshu Singh <sudhanshu1182abc@gmail.com>
2024-12-28 20:25:57 +0530

    url changed

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const DIRNAME = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://lms-d2xb.onrender.com",
     credentials : true
 }))
 
